3f738a80b7f96919a939dd4a1780a19422cc8c08
dingzi <906289674@qq.com>
2018-01-17 10:17:46 +0800

    更新

--- tasks/util/args.js
+++ tasks/util/args.js
@@ -1,32 +1,35 @@
 import yargs from 'yargs';
+
 const args = yargs
-	
-	.option('production',{
-		boolean:true,
-		default:false,
-		describe:'min all script'
-	})
 
-	.option('watch',{
-		boolean:true,
-		default:false,
-		describe:'watch all files'
-	})
+  .option('production',{
+    boolean:true,
+    default:false,
+    describe:'min all scripts'
+  })
 
-	.option('verbose',{
-		boolean:true,
-		default:false,
-		describe:'log'
-	})
+  .option('watch',{
+    boolean:true,
+    default:false,
+    describe:'watch all files'
+  })
 
-	.option('sourcemaps',{
-		describe:'force the creation of sroucemaps'
-	})
+  .option('verbose',{
+    boolean:true,
+    default:false,
+    describe:'log'
+  })
 
-	.option('port',{
-		string:true,
-		default:8081,
-		describe:'server port'
-	})
+  .option('sourcemaps',{
+    describe:'force the creation of sroucemaps'
+  })
 
-	.argv
+  .option('port',{
+    string:true,
+    default:8080,
+    describe:'server port'
+  })
+
+  .argv
+
+export default args;
